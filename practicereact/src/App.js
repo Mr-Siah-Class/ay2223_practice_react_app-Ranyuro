af692c04e81db0906903ccfacd38851a09292449
Ranyuro <adrianranullo.21@ichat.sp.edu.sg>
2022-07-14 14:50:13 +0800

    creation of all screens and adding routing to the app.js and making the build #2

--- practicereact/src/App.js
+++ practicereact/src/App.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import './App.css';
 
+import NavBar from './components/NavBar';
+
+import {
+  Route,
+  useParams,
+  Routes,
+  BrowserRouter
+} from "react-router-dom";
+
+import HomeScreen from './screens/HomeScreen';
+import ProductsScreen from './screens/ProductsScreen';
+import NoScreen from './screens/NoScreen';
+import FooterBar from './components/FooterBar';
+import SingleProductScreen from './screens/SingleProductScreen';
+
+const ProductIdWrapper = () => {
+  const { productId } = useParams();
+  return <SingleProductScreen productId={productId} />;
+};
+
 class App extends React.Component
 {
     constructor() {
@@ -11,7 +31,18 @@ class App extends React.Component
     {
         return (
             <div>
-                <h1>Hello React!</h1>
+                <NavBar/>
+
+                <BrowserRouter>
+                  <Routes>
+                    <Route exact path="/" element={<HomeScreen/>}/>
+                    <Route path="/products" element={<ProductsScreen/>}/>
+                    <Route path="/products/:productId" element={<ProductIdWrapper/>}/>
+                    <Route path="*" element={<NoScreen/>}/>
+                  </Routes>
+                </BrowserRouter>
+                
+                <FooterBar/>
             </div>
         );
     }
